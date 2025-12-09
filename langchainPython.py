cb6765f213ea29a80d255a9064863d624457a7b2
EWuerslin551 <ethan@wuerslin.net>
2025-12-09 19:23:31 +0000

    finished version one of the program

--- langchainPython.py
+++ langchainPython.py
@@ -4,7 +4,7 @@ from langchain_core.output_parsers import StrOutputParser
 from docx import Document #from AI
 
 DOCX_PATH = "/workspaces/Langchain-Project/Notes.docx"
-model = ChatOllama(model= "FactExtracter")
+model = ChatOllama(model= "Summeriser")
 def read_docx(path):
     doc = Document(path)            #|              
     full_text = []                  #|
@@ -16,23 +16,28 @@ prompt = ChatPromptTemplate.from_template(
     """ 
     You Are an Ai assistant that extracts factual information from documents. Your goal is to help  students by summerising their not document into key ideas and specific facts. if you do not deem there enough information for a good answer do not provide an answer, other wise aim for arround 3 ideas and 5 specific facts.
 
-    Answer mainly based off the context provided, but if the notes seem to match a major cource you cna include ideas from that section of the cource. Answer wiht only the details and facts, nothing else
+    Answer mainly based off the context provided, but if the notes seem to match a major cource you can include ideas from that section of the cource. Answer wiht only the details and facts, nothing else
 
-    Question:
-    {input}
 
     Context:
     {context}
 
     Answer:
     """)
+
 outputParser = StrOutputParser()
+chain = prompt | model | outputParser
+
+def aiResponce(docxText):
+    print("\nGenerating AI Responce...\n")
+    return chain.invoke({"context": docxText})
+
 
 
 
 #user interface:
 while True:
-    userInput = input("Path to Word Document (format: Example.docx) use 'exit' to quit: ")
+    userInput = input("\n\n\nPath to Word Document (format: Example.docx) use 'exit' to quit: ")
     if userInput.lower() == 'exit':
         break
     try:
@@ -41,5 +46,7 @@ while True:
     except:
         print("File not found. Please try again.")
         continue   
+    aiOutput = aiResponce(docxOutput)
+    print("\nAI Responce:\n" + aiOutput)
         
     
