f63e73d0d69a6f00a51d9a0bcdd0a855d085b642
EWuerslin551 <ethan@wuerslin.net>
2025-12-11 19:05:18 +0000

    add comments and cleaned up code

--- langchainPython.py
+++ langchainPython.py
@@ -1,10 +1,14 @@
+#importing libraries
 from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate 
 from langchain_core.output_parsers import StrOutputParser 
 from docx import Document #from AI
 
+#setting the outside resources to use
 DOCX_PATH = "/workspaces/Langchain-Project/Notes.docx"
 model = ChatOllama(model= "Summeriser")
+
+#taking the text out of the Word Document
 def read_docx(path):
     doc = Document(path)            #|              
     full_text = []                  #|
@@ -12,6 +16,7 @@ def read_docx(path):
         full_text.append(para.text) #|
     return '\n'.join(full_text)     #|
 
+#making the propt to be used later (slightly redundant)
 prompt = ChatPromptTemplate.from_template(
     """ 
     You Are an Ai assistant that extracts factual information from documents. Your goal is to help  students by summerising their not document into key ideas and specific facts. if you do not deem there enough information for a good answer do not provide an answer, other wise aim for arround 3 ideas and 5 specific facts.
@@ -25,27 +30,33 @@ prompt = ChatPromptTemplate.from_template(
     Answer:
     """)
 
+#setting paser and definigg the chain
 outputParser = StrOutputParser()
 chain = prompt | model | outputParser
 
+#goes through the chain
 def aiResponce(docxText):
     print("\nGenerating AI Responce...\n")
     return chain.invoke({"context": docxText})
 
 
-
-
 #user interface:
 while True:
     userInput = input("\n\n\nPath to Word Document (format: Example.docx) use 'exit' to quit: ")
+
+    #Checking if the user wants to exit
     if userInput.lower() == 'exit':
         break
+
+    #making sure the word Document Exists
     try:
         userInput = f"/workspaces/Langchain-Project/{userInput}"
         docxOutput = read_docx(userInput)
     except:
         print("File not found. Please try again.")
-        continue   
+        continue 
+
+    #using the funtion to give the user a result
     aiOutput = aiResponce(docxOutput)
     print("\nAI Responce:\n" + aiOutput)
         
